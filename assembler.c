b64374c8eed1c3da0a2ce4a4d02ee6e6b75c11e8
vincentlee9512 <li000053@stthomas.edu>
2018-10-08 17:21:57 -0500

    fixed segmentation fault

--- assembler.c
+++ assembler.c
@@ -5,9 +5,12 @@
 #include <string.h>
 
 //a structure that contains name and address for a label
+//value here store addresses for label; store actual value for any .fill variable
+//isDotFill contains boolean value to indicate that this is a .fill label or not
 struct Labels{
-	char labelName[6];
+	char* labelName;
 	int address;
+	int isDotFill;
 };
 
 void print_usage();
@@ -15,23 +18,18 @@ int containLabel(char* line);
 struct Labels createLabels(int addr, char* line);
 void print_label(struct Labels label);
 
+int main (int argc, char **argv){
+	char *inputFilename = (char*)malloc(sizeof(char)*256);
+	char *outputFilename = (char*)malloc(sizeof(char)*256);
 
-int main(int argc, char **argv){
-	char *inputFilename;
-	char *outputFilename;
-
-	//IFLAG, as boolean to ensure user enter a input file name
-	int IFLAG=0;
+	int IFLAG = 0;
 	int opt;
 
-	char line[100];
+	//IFLAG, as boolean to ensure user enter a input file name
+	char *line = (char*)malloc(sizeof(char)*100);
 	int address;
-
-	//Collection for the label in assembly language
-	//no sure the size at this point
-	int collectionSize = 30;
-	struct Labels collection[collectionSize];
-	int labelIterator = 0;
+	
+/////////////////     taking input argument ///////////////////////
 
 	while((opt = getopt(argc, argv, "i:o:")) != -1){
 		switch(opt){
@@ -54,37 +52,29 @@ int main(int argc, char **argv){
 
 	if(!IFLAG) {print_usage();}
 
+/////////////////////////////////////////////////////////////////	
+
+///////////////  read and write to files //////////////////////
 	FILE *rf = fopen(inputFilename, "r");
-	FILE *wf = fopen(outputFilename, "w");	
+	//FILE *wf = fopen(outputFilename, "w");
 
 	address = 0;
-	
-	while((fgets(line, 100, rf)) != NULL) {
-		//check if it is a label
-		if (containLabel(line)) {
-			//create a label using "Labels" structure
-			collection[labelIterator] = createLabels(address, line);
-			++labelIterator;
+
+	while((fgets(line, 100, rf)) != NULL){
+
+		if(containLabel(line)){
+			print_label(createLabels(address, line));
 		}
 
 		++address;
 	}
 
-	fclose(wf);
+	//fclose(wf);
 	fclose(rf);
 
-	labelIterator = 0;
-	while(labelIterator < collectionSize){
+///////////////////////////////////////////////////////////////
 
-		//if the labelName is empty, stop printing.
-		if(strlen(collection[collectionSize].labelName) == 0){ break;}
-
-		printf("Index: %d, Label: %s, Address: %d\n", labelIterator, collection[labelIterator].labelName,
-			   collection[labelIterator].address);
-		++labelIterator;
-	}
 }
-
 
 void print_usage(){
     printf("Usage: ./assembler -i program.as > program.mc | ./assembler -i program.ac -o program.mc | ./assembler -i program.ac\n");
@@ -92,7 +82,7 @@ void print_usage(){
 }
 
 int containLabel(char* line){
-	if (!isspace((int)line[0])){
+	if(!isspace((int)line[0])){
 		return 1;
 	}else{
 		return 0;
@@ -100,18 +90,19 @@ int containLabel(char* line){
 }
 
 struct Labels createLabels(int addr, char* line){
-	char* label;
+	int labelsize = 6;
+	char* label = (char*)malloc(sizeof(char)*(labelsize+1));	
 	struct Labels newLabel;
+	newLabel.labelName = (char*)malloc(sizeof(char)*(labelsize+1));
 
 	label = strtok(line, " ");
-
 	strcpy(newLabel.labelName, label);
-	newLabel.address = addr;
-	print_label(newLabel);
+	newLabel.address = addr;	
+
 	return newLabel;
 }
 
 void print_label(struct Labels label){
-	printf("Label: %s, Address: %d\n", label.labelName, label.address);
+	printf("Label: %s, address: %d\n", label.labelName, label.address);
 
 }
