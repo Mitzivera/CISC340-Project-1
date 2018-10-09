a2b1fbde56780bfb627d141d6c9865ad11970ef2
vincentlee9512 <li000053@stthomas.edu>
2018-10-09 13:59:47 -0500

    successfully get opcode from each lines

--- assembler.c
+++ assembler.c
@@ -18,6 +18,8 @@ void print_usage();
 int containLabel(char* line);
 void createLabels(int addr, char* line, struct Labels *labelArray, int index);
 void print_labelArray(struct Labels *labelArray, int length);
+unsigned long long int* translateLine(char *line);
+
 
 int main (int argc, char **argv){
 	char *inputFilename = (char*)malloc(sizeof(char)*256);
@@ -57,6 +59,7 @@ int main (int argc, char **argv){
 /////////////////////////////////////////////////////////////////	
 
 /////////////// 1st time  read and write to files //////////////////////
+//purpose: to get the length of the filename by counting the number of lines	
 	FILE *rf = fopen(inputFilename, "r");
 	numberOfLines = 0;
 	
@@ -65,13 +68,15 @@ int main (int argc, char **argv){
 	}
 ////////////////////////////////////////////////////////////////
 
+
+	//use the number of lines in the file to create the array of labels
 	struct Labels labelArray[numberOfLines];
 	int labelArrayIndex = 0;
 
 /////////////// 2nd time  read and write to files //////////////////////
-	rewind(rf);
-	//FILE *wf = fopen(outputFilename, "w");
+//purpose: to fill the labelArray with the label in assembly language	
 
+	rewind(rf);
 	address = 0;
 
 	while((fgets(line, 100, rf)) != NULL){
@@ -84,18 +89,83 @@ int main (int argc, char **argv){
 		++address;
 	}
 
-	print_labelArray(labelArray, labelArrayIndex);
-
-	//fclose(wf);
-	fclose(rf);
 
 ///////////////////////////////////////////////////////////////
 
+	//copy label array to a new array to remove the empty label, then free the pervious array.
+	struct Labels finalLabels[labelArrayIndex];
+	memcpy(finalLabels, labelArray, labelArrayIndex*sizeof(struct Labels));
+
+///////////// 3rd time read and write to files////////////////////////
+	
+	rewind(rf);
+	address = 0;
+	
+	while((fgets(line, 100, rf)) != NULL){
+		unsigned long long int *outputNumber = (unsigned long long int*)malloc(sizeof(unsigned long long int));	
+		
+		++address;	
+		
+		outputNumber = translateLine(line);
+	}
+
+
+
+	fclose(rf);
+	
+
+///////////////////////////////////////////////////////////////
 }
 
 void print_usage(){
     printf("Usage: ./assembler -i program.as > program.mc | ./assembler -i program.as -o program.mc | ./assembler -i program.as\n");
     exit(2);
+}
+
+unsigned long long int* translateLine(char *line){
+	char opcodes[9][5] = {"add", "nand", "lw", "sw", "beq", "jalr", "halt", "noop", ".fill"};		
+	char *strCopy = (char*)malloc(sizeof(char)*strlen(line));
+	strcpy(strCopy, line);
+	unsigned long long int *outputNumber = (unsigned long long int*)malloc(sizeof(unsigned long long int));
+	//this FLAG indicates the program found the opcode
+	int opcodeFLAG = 0;
+	//this index indicate which opcode is using
+	int opcodeIndex;
+
+	char *firstWord = (char*)malloc(6*sizeof(char));
+	strcpy(firstWord,strtok(strCopy, " \t\n\v\f\r"));
+
+	//This for loop tries to matched first word with one of the opcode
+	int i;
+	for(i=0; i<9; i++){
+		if(strcmp(firstWord, opcodes[i]) == 0){
+			//opcode matched
+			opcodeFLAG = 1;
+			opcodeIndex = i;
+			break;
+		}
+	}	
+
+
+	if(opcodeFLAG == 0){
+		char *secondWord = (char*)malloc(6*sizeof(char));
+		strcpy(secondWord, strtok(NULL, " \t\n\v\f\r"));
+		
+		for(i=0; i<9; i++){
+			if(strcmp(secondWord, opcodes[i]) == 0){
+				opcodeFLAG = 1;
+				opcodeIndex = i;
+				break;
+			}
+		}
+
+		printf("opcode :: %s, Line :: %s\n", opcodes[opcodeIndex], line);
+	}else{
+		printf("opcode :: %s, Line :: %s\n", opcodes[opcodeIndex], line);
+	}
+	*outputNumber = 1;
+
+	return outputNumber;
 }
 
 int containLabel(char* line){
@@ -118,18 +188,17 @@ void createLabels(int addr, char* line, struct Labels *labelArray, int index){
 	newLabel.labelName = (char*)malloc(sizeof(char)*(labelsize+1));
 	newLabel.isDotFill = 0;
 	newLabel.address = addr;
-	newLabel.value = 0;
 
-	label = strtok(line, " ");
+	label = strtok(line, " \t\n\v\f\r");
 	strcpy(newLabel.labelName, label);
 
 	//check if this is a .fill line
-	secWord = strtok(NULL, " ");	
+	secWord = strtok(NULL, " \t\n\v\f\r");	
 	//strcmp(char *a, char*b) returns 0 if these two string are equal, otherwise non-zero.
 	if(!strcmp(secWord, ".fill")){
 		newLabel.isDotFill = 1;
 		
-		thirdWord = strtok(NULL, " ");
+		thirdWord = strtok(NULL, " \t\n\v\f\r");
 	
 		immediateValue = atoi(thirdWord);
 
@@ -145,10 +214,10 @@ void createLabels(int addr, char* line, struct Labels *labelArray, int index){
 				//then we find the label in the labelArray, and take its address
 				int i;
 				for(i=0; i<index; i++){
-					printf("str1 : %s , str2 :%s\n", strtok(thirdWord, "\n"), labelArray[i].labelName);
-					if(strcmp(strtok(thirdWord, "\n"), labelArray[i].labelName) == 0){
+					if(strcmp(strtok(thirdWord, " \t\n\v\f\r"), labelArray[i].labelName) == 0){
 						//the label matched
 						newLabel.value = labelArray[i].address;
+						break;
 					}
 				}
 			}
